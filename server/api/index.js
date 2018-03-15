13d99911ab88142368967d1a390c09fcb121479c
Gabby Josebachvili <gabbyjose@gmail.com>
2018-03-15 18:17:58 -0400

    added game ending checks

--- server/api/index.js
+++ server/api/index.js
@@ -1,54 +1,60 @@
 const Router = require('express').Router();
 const Player = require('../db/models');
 const Round = require('../db/models');
-const { Op } = require('sequelize');
+const {
+  Op
+} = require('sequelize');
+const {
+  hasGameEnded,
+  didMafiaWin,
+  whoToSendBack
+} = require('../game.js');
 
 Router.get('/getInitialData', (req, res, next) => {
   const gameId = req.params.gameId
   Player.findOne({
-    where: {
-      cookieId: req.body.cookieId
-    }
-  })
-  .then(player => {
-    if (player.isMafia){
-      Player.findAll({
-        where: {
-          gameId : gameId,
-          role : 'Mafia'
-        }
-      })
-      .then(mafiaMembers => {
-        player.mafiaMembers = mafiaMembers
+      where: {
+        cookieId: req.body.cookieId
+      }
+    })
+    .then(player => {
+      if (player.isMafia) {
+        Player.findAll({
+            where: {
+              gameId: gameId,
+              role: 'Mafia'
+            }
+          })
+          .then(mafiaMembers => {
+            player.mafiaMembers = mafiaMembers
+            res.json(player)
+          })
+      } else {
         res.json(player)
-      })
-    }
-    else {
-      res.json(player)
-    }
-  })
+      }
+    })
 })
 
 Router.get('/getRoundData', (req, res, next) => {
   const gameId = req.params.gameId
   Round.findOne({
-    where: {
-      gameId: gameId,
-      isCurrent: true
-    }
-  })
-  .then(round => res.json(round))
+      where: {
+        gameId: gameId,
+        isCurrent: true
+      }
+    })
+    .then(round => res.json(round))
 })
 
 Router.get('/getAllPlayers', (req, res, next) => {
   const gameId = req.params.gameId
   Player.findAll({
-    attributes: ['name'],
-    where: {
-      gameId: gameId
-    }
-  })
-  .then(users => res.json(users))
+      attributes: ['name'],
+      where: {
+        gameId: gameId
+      }
+    })
+    .then(users => res.json(users))
 })
 
 
@@ -57,35 +63,33 @@ Router.get('/whoWon', (req, res, next) => {
   const gameId = req.params.gameId
 
   Player.findAll({
-    where: {
-      gameId: gameId,
-      role: 'Mafia',
-      isAlive: true
-    }
-  })
-  .then(mafias => aliveMafias = mafias.length)
+      where: {
+        gameId: gameId,
+        role: 'Mafia',
+        isAlive: true
+      }
+    })
+    .then(mafias => aliveMafias = mafias.length)
 
   Player.findAll({
-    where: {
-      gameId: gameId,
-      isAlive: true,
-      role: {
-        [Op.ne]:'Mafia'
+      where: {
+        gameId: gameId,
+        isAlive: true,
+        role: {
+          [Op.ne]: 'Mafia'
+        }
       }
-    }
-  })
-  .then(players => alivePlayers = players.length)
+    })
+    .then(players => alivePlayers = players.length)
 
 
-  if(hasGameEnded(aliveMafias, alivePlayers)){
-    if(didMafiaWin(aliveMafias)){
+  if (hasGameEnded(aliveMafias, alivePlayers)) {
+    if (didMafiaWin(aliveMafias)) {
       res.json('Mafia won')
-    }
-    else{
+    } else {
       res.json('Villagers won')
     }
-  }
-  else {
+  } else {
     //game continues, emit socket
   }
 })
@@ -93,8 +97,8 @@ Router.get('/whoWon', (req, res, next) => {
 
 Router.post('/newRound', (req, res, next) => {
   Round.create()
-  .then(round => round.setGame(req.params.gameId))
-  .then(currentRound => res.json(currentRound))
+    .then(round => round.setGame(req.params.gameId))
+    .then(currentRound => res.json(currentRound))
 })
 
 Router.put('/newRound', (req, res, next) => {
@@ -104,90 +108,83 @@ Router.put('/newRound', (req, res, next) => {
   let saved = req.body.saved || null;
   let died;
   const gameId = req.params.gameId
-    Round.findOne({
+  Round.findOne({
       where: {
         gameId: gameId,
         isCurrent: true
       }
     })
     .then(round => {
-      if(killed && !round.saved){
-        round.update({killed: killed})
-      }
-      else if (saved && !round.killed){
-        round.update({saved: saved})
-      }
-      else if (saved && round.killed) {
-        died = whoDies(round.killed, saved)
+      if (killed && !round.saved) {
         round.update({
-          saved: saved,
-          died: died
+          killed: killed
         })
-        .then(round => {
-          if (round.died !== 'none'){
-            Player.update({
-              isAlive: false
-            },
-            {
-              where: {
-                gameId: gameId,
-                name: round.died
-              }
-            })
-          }
-        })
-        .then(() => {
-//          socket.emit('updateData')
-        })
-      }
-      else if (killed && round.saved){
-        died = whoDies(killed, round.saved)
+      } else if (saved && !round.killed) {
         round.update({
-          killed: killed,
-          died: died
+          saved: saved
         })
-        .then(() => {
-//        socket.emit('updateData')
-        })
+      } else if (saved && round.killed) {
+        person = whoToSendBack(round.killed, saved)
+        const whoDied = person.saved ? null : person.killed
+        round.update({
+            saved: saved,
+            died: whoDied,
+            isCurrent: false
+          })
+          .then(round => {
+            if (round.died) {
+              Player.update({
+                isAlive: false
+              }, {
+                where: {
+                  gameId: gameId,
+                  name: round.died
+                }
+              })
+            }
+            return round;
+          })
+          .then(round => {
+            Game.findById(gameId)
+              .then(game => {
+                if (game.hasEnded()) {
+                  res.json(game.Winner)
+                } else {
+                  //socket.emit('updateData') and if someone died (if round.died is truthy), send back round.died bc it's their name; else return round.saved and that's the name of who was saved
+                }
+              })
+          })
+      } else if (killed && round.saved) {
+        person = whoToSendBack(killed, round.saved)
+        const whoDied = person.saved ? null : person.killed
+        round.update({
+            killed: killed,
+            died: whoDied,
+            isCurrent: false
+          })
+          .then(round => {
+            if (round.died) {
+              Player.update({
+                isAlive: false
+              }, {
+                where: {
+                  gameId: gameId,
+                  name: round.died
+                }
+              })
+            }
+            return round;
+          })
+          .then(round => {
+            Game.findById(gameId)
+              .then(game => {
+                if (game.hasEnded()) {
+                  res.json(game.Winner)
+                } else {
+                  //socket.emit('updateData') and if someone died (if round.died is truthy), send back round.died bc it's their name; else return round.saved and that's the name of who was saved
+                }
+              })
+          })
       }
     })
-  })
-
-  // else if (saved){
-  //   Round.findOne({
-  //     // attributes: ['saved'],
-  //     where: {
-  //       gameId: gameId,
-  //       isCurrent: true
-  //     }
-  //   })
-  //   .then(round => {
-  //     if(!round.killed){
-  //       round.update({saved: saved})
-  //     }
-  //     else {
-  //       //calculate stuff and emit the update data socket
-  //     }
-  //   })
-  // }
-
-
-  // if (killed === saved){
-  //   let died = 'none';
-  // }
-  // else {
-  //   let died = killed;
-  // }
-  // const gameId = req.params.gameId
-
-  // Round.findOne({
-  //   where: {
-  //     gameId: gameId,
-  //     isCurrent: true
-  //   }
-  // })
-  // .then(round => round.update({
-  //   killed,
-  //   saved,
-  //   died
-  // }))
+})
