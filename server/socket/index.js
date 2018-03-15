af7c3097570dfc94fef26838e8f7d6354c8a9c7d
Dani de Waal <danidewaal@aol.com>
2018-03-15 16:27:15 -0400

    server skeleton

---
+++ server/socket/index.js
@@ -0,0 +1,11 @@
+module.exports = io => {
+  io.on("connection", socket => {
+    console.log(
+      `A socket connection to the server has been made: ${socket.id}`
+    );
+
+    socket.on("disconnect", () => {
+      console.log(`Connection ${socket.id} is no longer connected!`);
+    });
+  });
+};
