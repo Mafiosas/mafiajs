f4c2f0390a8407abec1e90121ba2c0f4edd1249b
Dani de Waal <danidewaal@aol.com>
2018-03-15 17:10:06 -0400

    fixed shuffle function

--- server/game.js
+++ server/game.js
@@ -2,19 +2,19 @@
 
 //fisher yates shuffle:
 function shuffle(playersArray) {
-  let counter = array.length;
+  let counter = playersArray.length;
 
   while (counter > 0) {
     let index = Math.floor(Math.random() * counter);
 
     counter--;
 
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
+    let temp = playersArray[counter];
+    playersArray[counter] = playersArray[index];
+    playersArray[index] = temp;
   }
 
-  return array;
+  return playersArray;
 }
 
 function assignRoles(shuffled) {
@@ -28,10 +28,10 @@ function assignRoles(shuffled) {
   const doc = [shuffled.pop()];
   const civ = shuffled;
 
-  result[mafia] = maf;
-  result[detective] = det;
-  result[doctor] = doc;
-  result[civilian] = civ;
+  result["mafia"] = maf;
+  result["detective"] = det;
+  result["doctor"] = doc;
+  result["civilian"] = civ;
 
   return result;
 }
