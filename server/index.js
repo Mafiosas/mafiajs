79439756b763f3e0dab5ee54807f4a144181da13
Dani de Waal <danidewaal@aol.com>
2018-03-17 12:55:46 -0400

    tokbox stuff

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const socketio = require("socket.io");
 const routes = require("./api");
+const OpenTok = require("opentok");
+const Game = require("./db/models/game");
+
 module.exports = app;
 
 // logging middleware
@@ -41,6 +44,16 @@ const createApp = () => {
       });
     });
   }
+
+  const opentok = new OpenTok(
+    "46081452",
+    "3d9f569b114ccfa5ae1e545230656c6adb5465d3"
+  );
+  opentok.createSession(function(err, session) {
+    if (err) return console.log(err);
+
+    app.set("sessionId", session.sessionId);
+  });
 };
 
 const startListening = () => {
@@ -61,3 +74,35 @@ if (require.main === module) {
 } else {
   createApp();
 }
+
+// // The session will the OpenTok Media Router:
+// opentok.createSession({mediaMode:"routed"}, function(err, session) {
+//   if (err) return console.log(err);
+
+//   // save the sessionId
+//   db.save('session', session.sessionId, done);
+// });
+
+// // A Session with a location hint
+// opentok.createSession({location:'12.34.56.78'}, function(err, session) {
+//   if (err) return console.log(err);
+
+//   // save the sessionId
+//   db.save('session', session.sessionId, done);
+// });
+
+// // A Session with an automatic archiving
+// opentok.createSession({mediaMode:'routed', archiveMode:'always'}, function(err, session) {
+//   if (err) return console.log(err);
+
+//   // save the sessionId
+//   db.save('session', session.sessionId, done);
+// });
+
+// // Generate a Token from just a sessionId (fetched from a database)
+// token = opentok.generateToken(sessionId);
+
+// //to disconnect participant from OpenTok session
+// opentok.forceDisconnect(sessionId, connectionId, function(error) {
+//   if (error) return console.log("error:", error);
+// });
