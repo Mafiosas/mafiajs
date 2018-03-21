d8e7e65563295a4a71c6742300fe9264fa63eaf9
Dani de Waal <danidewaal@aol.com>
2018-03-21 14:30:11 -0400

    dark logic

--- client/components/SelectPlayerForm.jsx
+++ client/components/SelectPlayerForm.jsx
@@ -3,31 +3,33 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getPlayers } from "../store";
 
-class SelectPlayerForm extends Component {
-  render() {
-    //need to pass a handleSubmit down from parent component
-    const { handleSubmit, players } = props;
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Pick A Player:</label>
-          <select name="selectPlayers">
-            {players.map(player => {
+const SelectPlayerForm = props => {
+  //need to pass a handleSubmit down from parent component
+  const { handleSubmit, players } = props;
+  console.log("players inside form", players);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Pick A Player:</label>
+        <select className="browser-default" name="selectPlayers">
+          {players.length &&
+            players.map(player => {
               return (
                 <option key={player.id} value={player.id}>
                   {player.name}
                 </option>
               );
             })}
-          </select>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-  }
-}
+        </select>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
-const mapState = ({ players }) => ({ players });
-const mapDispatch = { getPlayers };
+export default SelectPlayerForm;
 
-export default connect(mapState, mapDispatch)(SelectPlayerForm);
+// const mapState = ({ players }) => ({ players });
+// const mapDispatch = { getPlayers };
+
+// export default connect(mapState, mapDispatch)(SelectPlayerForm);
