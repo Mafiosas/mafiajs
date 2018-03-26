2bcee202f25edbb92cc78a0b6fd361f27bd56646
Brittany Chu <brittany.s.chu@gmail.com>
2018-03-26 19:36:59 -0400

    disables submit button

--- public/bundle.js
+++ public/bundle.js
@@ -1456,51 +1456,108 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-var RoomForm = function RoomForm(props) {
-  var handleSubmit = props.handleSubmit;
-  return _react.default.createElement("div", {
-    className: "container"
-  }, _react.default.createElement(_reactSound.default, {
-    url: "ayasiikuuki.MP3",
-    loop: "true",
-    playStatus: _reactSound.default.status.PLAYING
-  }), _react.default.createElement(_reactSound.default, {
-    url: "darkshadow.mp3",
-    loop: "true",
-    playStatus: _reactSound.default.status.PLAYING
-  }), _react.default.createElement("form", {
-    id: "room-form",
-    onSubmit: handleSubmit
-  }, _react.default.createElement("h4", null, "Let's Play A Game..."), _react.default.createElement("div", null, _react.default.createElement("label", null, "Room Name:"), _react.default.createElement("input", {
-    name: "roomName",
-    type: "text"
-  })), _react.default.createElement("div", null, _react.default.createElement("label", null, "Password (optional):"), _react.default.createElement("input", {
-    name: "password",
-    type: "text"
-  })), _react.default.createElement("div", null, _react.default.createElement("label", null, "Number of Players (defaults to 6)"), _react.default.createElement("select", {
-    className: "browser-default",
-    name: "numPlayers"
-  }, _react.default.createElement("option", {
-    value: "6"
-  }, "6"), _react.default.createElement("option", {
-    value: "7"
-  }, "7"), _react.default.createElement("option", {
-    value: "8"
-  }, "8"), _react.default.createElement("option", {
-    value: "9"
-  }, "9"), _react.default.createElement("option", {
-    value: "10"
-  }, "10"), _react.default.createElement("option", {
-    value: "11"
-  }, "11"), _react.default.createElement("option", {
-    value: "12"
-  }, "12"))), _react.default.createElement("div", null, _react.default.createElement("label", null, "Player Name:"), _react.default.createElement("input", {
-    name: "name"
-  }), _react.default.createElement("button", {
-    className: "waves-effect waves-light btn",
-    type: "submit"
-  }, "Create Game"))));
-};
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+var RoomForm =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(RoomForm, _Component);
+
+  function RoomForm(props) {
+    var _this;
+
+    _classCallCheck(this, RoomForm);
+
+    _this = _possibleConstructorReturn(this, (RoomForm.__proto__ || Object.getPrototypeOf(RoomForm)).call(this, props));
+    _this.state = {
+      input: "",
+      disabled: true
+    };
+    _this.handleChange = _this.handleChange.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(RoomForm, [{
+    key: "handleChange",
+    value: function handleChange(event) {
+      var target = event.target.value.length;
+      if (target > 0) this.setState({
+        disabled: false
+      });else this.setState({
+        disabled: true
+      });
+      this.setState({
+        input: event.target.value
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var handleSubmit = this.props.handleSubmit;
+      return _react.default.createElement("div", {
+        className: "container"
+      }, _react.default.createElement(_reactSound.default, {
+        url: "ayasiikuuki.MP3",
+        loop: "true",
+        playStatus: _reactSound.default.status.PLAYING
+      }), _react.default.createElement(_reactSound.default, {
+        url: "darkshadow.mp3",
+        loop: "true",
+        playStatus: _reactSound.default.status.PLAYING
+      }), _react.default.createElement("h2", null, "Let's Play A Game..."), _react.default.createElement("form", {
+        id: "room-form",
+        onSubmit: handleSubmit
+      }, _react.default.createElement("div", null, _react.default.createElement("label", null, "Room Name:"), _react.default.createElement("input", {
+        name: "roomName",
+        type: "text",
+        placeholder: "Enter a room name"
+      })), _react.default.createElement("div", null, _react.default.createElement("label", null, "Password (optional):"), _react.default.createElement("input", {
+        name: "password",
+        type: "text",
+        placeholder: "Enter a password"
+      })), _react.default.createElement("div", null, _react.default.createElement("label", null, "Number of Players (defaults to 6)"), _react.default.createElement("select", {
+        className: "browser-default",
+        name: "numPlayers"
+      }, _react.default.createElement("option", {
+        value: "6"
+      }, "6"), _react.default.createElement("option", {
+        value: "7"
+      }, "7"), _react.default.createElement("option", {
+        value: "8"
+      }, "8"), _react.default.createElement("option", {
+        value: "9"
+      }, "9"), _react.default.createElement("option", {
+        value: "10"
+      }, "10"), _react.default.createElement("option", {
+        value: "11"
+      }, "11"), _react.default.createElement("option", {
+        value: "12"
+      }, "12"))), _react.default.createElement("div", null, _react.default.createElement("label", null, "Player Name:"), _react.default.createElement("input", {
+        name: "name",
+        onChange: this.handleChange,
+        placeholder: "Enter your name"
+      }), _react.default.createElement("button", {
+        className: "waves-effect waves-light btn",
+        type: "submit",
+        disabled: this.state.disabled
+      }, "Create Game"))));
+    }
+  }]);
+
+  return RoomForm;
+}(_react.Component);
 
 var mapCreateDispatch = function mapCreateDispatch(dispatch, ownProps) {
   return {
