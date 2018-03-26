76325dbbbb286f90853c8ade6a729c400f3e7db2
Brittany Chu <brittany.s.chu@gmail.com>
2018-03-26 19:20:51 -0400

    game list updates immediately

--- client/store/games.js
+++ client/store/games.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 /* ACTION TYPES */
 const GET_GAMES = "GET_GAMES";
+const ADD_GAME = "ADD_GAME";
 
 /* INITIAL STATE */
 const defaultGames = [];
 
 /* ACTION CREATORS */
 const getGames = games => ({ type: GET_GAMES, games });
+export const addGame = game => ({ type: ADD_GAME, game });
 
 /* THUNK CREATORS */
 export const fetchGames = () => dispatch =>
@@ -21,6 +23,8 @@ export default function(state = defaultGames, action) {
   switch (action.type) {
     case GET_GAMES:
       return action.games;
+    case ADD_GAME:
+      return [...state, action.game];
     default:
       return state;
   }
