09ca9a45fee3b654eb6936d3e8735115166705f5
Dani de Waal <danidewaal@aol.com>
2018-06-27 17:06:20 -0400

    removed unused vars

--- client/components/GameRoom.jsx
+++ client/components/GameRoom.jsx
@@ -1,21 +1,18 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import socket from "../socket";
-import { OTSession, OTPublisher, OTStreams, OTSubscriber } from "opentok-react";
-import MafiaSelectForm from "./MafiaSelectForm.jsx";
-import DetectiveSelectForm from "./DetectiveSelectForm.jsx";
-import DoctorSelectForm from "./DoctorSelectForm.jsx";
-import DayTimeForm from "./DayTimeForm.jsx";
-import sounds from "./sound.jsx";
-import Timer from "./Timer.jsx";
-import Sound from "react-sound";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import socket from '../socket';
+import { OTSession, OTPublisher, OTStreams, OTSubscriber } from 'opentok-react';
+import MafiaSelectForm from './MafiaSelectForm.jsx';
+import DetectiveSelectForm from './DetectiveSelectForm.jsx';
+import DoctorSelectForm from './DoctorSelectForm.jsx';
+import DayTimeForm from './DayTimeForm.jsx';
+import Timer from './Timer.jsx';
+import Sound from 'react-sound';
 
 import {
   fetchGame,
-  user,
-  joinExistingGame,
   getMe,
   getOnlyMe,
   fetchFacts,
@@ -24,23 +21,23 @@ import {
   resetVotes,
   updateUser,
   removePlayer
-} from "../store";
+} from '../store';
 
 class GameRoom extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      time: "",
+      time: '',
       error: null,
-      connection: "Connecting",
+      connection: 'Connecting',
       publishVideo: true,
-      resultMessage: "",
-      detective: "",
-      winner: "",
+      resultMessage: '',
+      detective: '',
+      winner: '',
       timerToggle: 0,
-      playStatusNight: "STOPPED",
-      playStatusDay: "STOPPED",
+      playStatusNight: 'STOPPED',
+      playStatusDay: 'STOPPED',
       died: {}
     };
 
@@ -62,25 +59,25 @@ class GameRoom extends Component {
 
     this.sessionEventHandlers = {
       sessionConnected: () => {
-        this.setState({ connection: "Connected" });
+        this.setState({ connection: 'Connected' });
       },
       sessionDisconnected: () => {
-        this.setState({ connection: "Disconnected" });
+        this.setState({ connection: 'Disconnected' });
       },
       sessionReconnected: () => {
-        this.setState({ connection: "Reconnected" });
+        this.setState({ connection: 'Reconnected' });
       },
       sessionReconnecting: () => {
-        this.setState({ connection: "Reconnecting" });
+        this.setState({ connection: 'Reconnecting' });
       }
     };
 
     this.publisherEventHandlers = {
       accessDenied: () => {
-        console.log("User denied access to media source");
+        console.log('User denied access to media source');
       },
       streamCreated: () => {
-        console.log("Publisher stream created");
+        console.log('Publisher stream created');
       },
       streamDestroyed: ({ reason }) => {
         console.log(`Publisher stream destroyed because: ${reason}`);
@@ -89,10 +86,10 @@ class GameRoom extends Component {
 
     this.subscriberEventHandlers = {
       videoEnabled: () => {
-        console.log("Subscriber video enabled");
+        console.log('Subscriber video enabled');
       },
       videoDisabled: () => {
-        console.log("Subscriber video disabled");
+        console.log('Subscriber video disabled');
       }
     };
   }
@@ -102,76 +99,76 @@ class GameRoom extends Component {
     this.props.loadData();
     this.props.loadFacts();
     this.props.loadDeaths();
-    socket.on("getRoles", this.getRoles);
-    socket.on("dark", this.dark);
-    socket.on("daytime", payload => {
+    socket.on('getRoles', this.getRoles);
+    socket.on('dark', this.dark);
+    socket.on('daytime', payload => {
       this.props.loadMe();
       this.daytime(payload);
     });
-    socket.on("role", payload => this.assignRole(payload));
-    socket.on("DetectiveChoice", choice => {
-      const answer = choice ? "right" : "wrong";
+    socket.on('role', payload => this.assignRole(payload));
+    socket.on('DetectiveChoice', choice => {
+      const answer = choice ? 'right' : 'wrong';
       this.detectiveAnswer(answer);
     });
-    socket.on("myVote", dataVal => {
+    socket.on('myVote', dataVal => {
       this.props.releaseVote(dataVal);
     });
-    socket.on("votesData", (votedOut, wasMafia) => {
+    socket.on('votesData', (votedOut, wasMafia) => {
       this.giveVotesData(votedOut, wasMafia);
     });
-    socket.on("resetVotes", () => {
+    socket.on('resetVotes', () => {
       this.voteReset();
     });
-    socket.on("gameOver", data => {
+    socket.on('gameOver', data => {
       this.gameOver(data);
     });
-    socket.on("owl", () => {
+    socket.on('owl', () => {
       this.owl();
     });
-    socket.on("rooster", () => {
+    socket.on('rooster', () => {
       this.rooster();
     });
   }
 
   componentWillUnmount() {
     this.setState({
-      time: "",
+      time: '',
       error: null,
-      connection: "Connecting",
+      connection: 'Connecting',
       publishVideo: true,
-      resultMessage: "",
-      detective: "",
-      winner: "",
+      resultMessage: '',
+      detective: '',
+      winner: '',
       timerToggle: 0,
-      playStatusNight: "STOPPED",
-      playStatusDay: "STOPPED",
+      playStatusNight: 'STOPPED',
+      playStatusDay: 'STOPPED',
       died: {}
     });
-    socket.removeListener("getRoles");
-    socket.removeListener("dark");
-    socket.removeListener("daytime");
-    socket.removeListener("role");
-    socket.removeListener("DetectiveRight");
-    socket.removeListener("DetectiveWrong");
-    socket.removeListener("myVote");
-    socket.removeListener("votesData");
-    socket.removeListener("resetVotes");
-    socket.removeListener("gameOver");
-    socket.removeListener("owl");
-    socket.removeListener("rooster");
+    socket.removeListener('getRoles');
+    socket.removeListener('dark');
+    socket.removeListener('daytime');
+    socket.removeListener('role');
+    socket.removeListener('DetectiveRight');
+    socket.removeListener('DetectiveWrong');
+    socket.removeListener('myVote');
+    socket.removeListener('votesData');
+    socket.removeListener('resetVotes');
+    socket.removeListener('gameOver');
+    socket.removeListener('owl');
+    socket.removeListener('rooster');
   }
 
   owl() {
-    this.setState({ playStatusNight: "PLAYING" });
+    this.setState({ playStatusNight: 'PLAYING' });
     setTimeout(() => {
-      this.setState({ playStatusNight: "STOPPED" });
+      this.setState({ playStatusNight: 'STOPPED' });
     }, 2000);
   }
 
   rooster() {
-    this.setState({ playStatusDay: "PLAYING" });
+    this.setState({ playStatusDay: 'PLAYING' });
     setTimeout(() => {
-      this.setState({ playStatusDay: "STOPPED" });
+      this.setState({ playStatusDay: 'STOPPED' });
     }, 2000);
   }
   getRoles() {
@@ -184,8 +181,8 @@ class GameRoom extends Component {
 
   daytime(payload) {
     this.setState({
-      time: "Day",
-      detective: ""
+      time: 'Day',
+      detective: ''
     });
 
     let num;
@@ -202,7 +199,7 @@ class GameRoom extends Component {
       num = this.state.died.id % this.props.deaths.length;
       let death = this.props.deaths[num].storyForKilled;
       this.props.updateUser({
-        role: "Dead",
+        role: 'Dead',
         isAlive: false
       });
       this.setState({
@@ -251,18 +248,18 @@ class GameRoom extends Component {
   }
 
   gameStart() {
-    socket.emit("gameStart", this.props.game.id);
+    socket.emit('gameStart', this.props.game.id);
   }
 
   dark() {
     this.setState({
-      time: "Night",
+      time: 'Night',
       timerToggle: 30
     });
   }
 
   darkOverMafia(killedId) {
-    socket.emit("darkData", {
+    socket.emit('darkData', {
       killed: killedId,
       gameId: this.props.game.id,
       user: this.props.user
@@ -270,7 +267,7 @@ class GameRoom extends Component {
   }
 
   darkOverDetective(guessId) {
-    socket.emit("villagerChoice", {
+    socket.emit('villagerChoice', {
       guess: guessId,
       gameId: this.props.game.id,
       user: this.props.user
@@ -278,7 +275,7 @@ class GameRoom extends Component {
   }
 
   darkOverDoctor(savedId) {
-    socket.emit("villagerChoice", {
+    socket.emit('villagerChoice', {
       saved: savedId,
       gameId: this.props.game.id,
       user: this.props.user
@@ -286,7 +283,7 @@ class GameRoom extends Component {
   }
 
   sendVotes(votes) {
-    socket.emit("daytimeVotes", votes);
+    socket.emit('daytimeVotes', votes);
   }
 
   voteReset() {
@@ -296,14 +293,14 @@ class GameRoom extends Component {
   giveVotesData(name, wasMafia) {
     const player = this.props.players.find(el => el.name === name);
     this.props.removePlayerFromStore(player.id);
-    this.setState({ time: "day2", timerToggle: 30 });
+    this.setState({ time: 'day2', timerToggle: 30 });
     if (this.props.user.name === name && !wasMafia) {
       this.setState({
         resultMessage:
-          "The group guessed you to be the Mafia and were wrong! You are now out of the game."
+          'The group guessed you to be the Mafia and were wrong! You are now out of the game.'
       });
       this.props.updateUser({
-        role: "Dead",
+        role: 'Dead',
         isAlive: false
       });
     } else if (this.props.user.name !== name && !wasMafia) {
@@ -313,10 +310,10 @@ class GameRoom extends Component {
     } else if (this.props.user.name === name && wasMafia) {
       this.setState({
         resultMessage:
-          "The group was right! They guessed you as Mafia and you have been voted out the game."
+          'The group was right! They guessed you as Mafia and you have been voted out the game.'
       });
       this.props.updateUser({
-        role: "Dead",
+        role: 'Dead',
         isAlive: false
       });
     } else if (this.props.user.name !== name && wasMafia) {
@@ -331,7 +328,7 @@ class GameRoom extends Component {
   };
 
   onPublish = () => {
-    console.log("Publish Success");
+    console.log('Publish Success');
   };
 
   onPublishError = error => {
@@ -339,7 +336,7 @@ class GameRoom extends Component {
   };
 
   onSubscribe = () => {
-    console.log("Subscribe Success");
+    console.log('Subscribe Success');
   };
 
   onSubscribeError = error => {
@@ -356,11 +353,9 @@ class GameRoom extends Component {
 
     const token = user.token;
 
-    const apiKey = "46093032";
+    const apiKey = '46093032';
     const {
       detective,
-      error,
-      connection,
       publishVideo,
       time,
       resultMessage,
@@ -372,7 +367,7 @@ class GameRoom extends Component {
     const index = Math.floor(Math.random() * Math.floor(facts.length - 1));
 
     const messageToMafia =
-      "Mafia, please make yourselves known to each other! You can see and hear everyone, they cannot see you. Discuss your plans freely...";
+      'Mafia, please make yourselves known to each other! You can see and hear everyone, they cannot see you. Discuss your plans freely...';
 
     return (
       <div>
@@ -385,7 +380,7 @@ class GameRoom extends Component {
               <div id="top-row" className="row">
                 <div className="col s3">
                   {time &&
-                    time !== "day2" && (
+                    time !== 'day2' && (
                       <div>
                         <h4>Time:</h4>
                         <h6>{time}</h6>
@@ -393,20 +388,20 @@ class GameRoom extends Component {
                     )}
 
                   {time &&
-                    time === "day2" && (
+                    time === 'day2' && (
                       <div>
                         <h4>Time:</h4>
                         <h6>Day</h6>
                       </div>
                     )}
                   <br />
-                  {time === "day2" && (
+                  {time === 'day2' && (
                     <div className="countdown">
                       <h5>Countdown: </h5>
                       <Timer timer={this.state.timerToggle} />
                     </div>
                   )}
-                  {time === "Night" && (
+                  {time === 'Night' && (
                     <div className="countdown">
                       <h5>Countdown: </h5>
                       <Timer timer={this.state.timerToggle} />
@@ -415,7 +410,7 @@ class GameRoom extends Component {
                 </div>
                 <br />
 
-                {time === "Night" && <img id="owl" src="/wideMoon.gif" />}
+                {time === 'Night' && <img id="owl" src="/wideMoon.gif" />}
                 <div className="col s9">
                   {!user.role &&
                     user.creator &&
@@ -427,21 +422,21 @@ class GameRoom extends Component {
                         Ready? Click here to begin your game of MAFIA
                       </button>
                     )}
-                  {time === "Day" && <h5>{resultMessage}</h5>}
-                  {time === "day2" && <h5>{resultMessage}</h5>}
+                  {time === 'Day' && <h5>{resultMessage}</h5>}
+                  {time === 'day2' && <h5>{resultMessage}</h5>}
 
                   {user.role &&
-                    time === "Night" &&
-                    user.role !== "Dead" && (
+                    time === 'Night' &&
+                    user.role !== 'Dead' && (
                       <h2 id="role">You're the {user.role}</h2>
                     )}
                   {user.role &&
-                    time === "Night" &&
-                    user.role === "Dead" && (
+                    time === 'Night' &&
+                    user.role === 'Dead' && (
                       <h3>Boo..you're out of the game</h3>
                     )}
-                  {time === "Night" &&
-                    user.role === "Detective" &&
+                  {time === 'Night' &&
+                    user.role === 'Detective' &&
                     detective && <h3>Detective, you were {detective}</h3>}
                 </div>
               </div>
@@ -464,8 +459,8 @@ class GameRoom extends Component {
                   ) : null}
                 </div>
                 <div className="col s9">
-                  {time === "Day" &&
-                    user.role !== "Dead" && (
+                  {time === 'Day' &&
+                    user.role !== 'Dead' && (
                       <div>
                         <h3>Who do you think the Mafia is?</h3>
 
@@ -475,8 +470,8 @@ class GameRoom extends Component {
                         />
                       </div>
                     )}
-                  {time === "Night" &&
-                    user.role === "Doctor" && (
+                  {time === 'Night' &&
+                    user.role === 'Doctor' && (
                       <div>
                         <h4>Choose who to save</h4>
                         <DoctorSelectForm
@@ -485,8 +480,8 @@ class GameRoom extends Component {
                         />
                       </div>
                     )}
-                  {time === "Night" &&
-                    user.role === "Detective" &&
+                  {time === 'Night' &&
+                    user.role === 'Detective' &&
                     !detective && (
                       <div>
                         <h4>Choose who you suspect is Mafia</h4>
@@ -498,8 +493,8 @@ class GameRoom extends Component {
                       </div>
                     )}
 
-                  {time === "Night" &&
-                    user.role === "Lead Mafia" && (
+                  {time === 'Night' &&
+                    user.role === 'Lead Mafia' && (
                       <div>
                         <h5>{messageToMafia}</h5>
                         <br />
@@ -513,15 +508,15 @@ class GameRoom extends Component {
                         />
                       </div>
                     )}
-                  {time === "Night" &&
-                    user.role === "Mafia" && (
+                  {time === 'Night' &&
+                    user.role === 'Mafia' && (
                       <div>
                         <h5>{messageToMafia}</h5>
                       </div>
                     )}
 
-                  {time === "Night" &&
-                    user.role === "Civilian" && (
+                  {time === 'Night' &&
+                    user.role === 'Civilian' && (
                       <div>
                         <p>{facts[index].fact}</p>
                       </div>
@@ -599,20 +594,20 @@ class GameRoom extends Component {
                           width: 200,
                           height: 200,
                           subscribeToAudio:
-                            time === "Night" &&
+                            time === 'Night' &&
                             user.role &&
-                            user.role !== "Mafia" &&
-                            user.role !== "Lead Mafia" &&
-                            user.role !== "Dead" &&
+                            user.role !== 'Mafia' &&
+                            user.role !== 'Lead Mafia' &&
+                            user.role !== 'Dead' &&
                             !winner
                               ? false
                               : true,
                           subscribeToVideo:
-                            time === "Night" &&
+                            time === 'Night' &&
                             user.role &&
-                            user.role !== "Mafia" &&
-                            user.role !== "Lead Mafia" &&
-                            user.role !== "Dead" &&
+                            user.role !== 'Mafia' &&
+                            user.role !== 'Lead Mafia' &&
+                            user.role !== 'Dead' &&
                             !winner
                               ? false
                               : true
@@ -679,7 +674,12 @@ const mapDispatch = (dispatch, ownProps) => {
   };
 };
 
-export default withRouter(connect(mapState, mapDispatch)(GameRoom));
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(GameRoom)
+);
 
 /* PROP TYPES */
 GameRoom.propTypes = {
